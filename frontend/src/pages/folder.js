ec53fd04d6fea32cb5c3677493ad18d0e841c6f9
itc-s23011 <s23011@std.it-college.ac.jp>
2024-07-30 10:55:30 +0900

    フォルダページ作成

---
+++ frontend/src/pages/folder.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Folder = () => {
+	return (
+
+		<div style={{width: 1280, height: 832, position: 'relative', background: 'white'}}>
+		<div style={{left: 576, top: 55, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>フォルダ</div>
+		<div className="Group14" style={{width: 1025, height: 365, left: 127, top: 366, position: 'absolute'}}>
+		<div className="Rectangle28" style={{width: 1025, height: 365, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 20, border: '1px black solid'}} />
+		<div className="Line2" style={{width: 870, height: 0, left: 78, top: 125, position: 'absolute', border: '1px black solid'}}></div>
+		<div className="Line3" style={{width: 870, height: 0, left: 78, top: 244, position: 'absolute', border: '1px black solid'}}></div>
+		<div className="Ellipse1" style={{width: 29, height: 29, left: 30, top: 56, position: 'absolute', background: '#70CBF2', borderRadius: 9999}} />
+		<div className="Ellipse2" style={{width: 29, height: 29, left: 30, top: 168, position: 'absolute', background: '#70CBF2', borderRadius: 9999}} />
+		<div className="Ellipse3" style={{width: 29, height: 29, left: 30, top: 280, position: 'absolute', background: '#70CBF2', borderRadius: 9999}} />
+		<div style={{left: 123, top: 47, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>消耗品</div>
+		<div style={{left: 948, top: 57, position: 'absolute', color: '#8E8585', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>＞</div>
+		<div style={{left: 948, top: 168, position: 'absolute', color: '#8E8585', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>＞</div>
+		<div style={{left: 948, top: 280, position: 'absolute', color: '#8E8585', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>＞</div>
+		<div style={{left: 123, top: 158, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>食品</div>
+		<div style={{left: 123, top: 272, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>その他</div>
+		</div>
+		<div className="Rectangle29" style={{width: 1025, height: 112, left: 127, top: 189, position: 'absolute', background: 'white', borderRadius: 20, border: '1px black solid'}} />
+		<div style={{left: 528, top: 225, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>フォルダを作成</div>
+		</div>
+
+	);
+};
+export default Folder;
