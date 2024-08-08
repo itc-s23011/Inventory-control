c53115b4a72ab2259688a9ff21887edf36a3db54
itc-s23006 <s23006@std.it-college.ac.jp>
2024-08-08 11:26:20 +0900

    コメントを追加。

--- frontend/src/pages/inventoryList.js
+++ frontend/src/pages/inventoryList.js
@@ -38,7 +38,7 @@ const InventoryList = () => {
   };
 
   const handleAddItem = () => {
-    router.push("/add-item");
+    router.push("/add-item"); // ＋ボタンをクリックすると、とぶUrl。
   };
 
   return (
@@ -55,7 +55,7 @@ const InventoryList = () => {
             <Link href="/">
               <div style={{ color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '400', cursor: 'pointer' }}>←</div>
             </Link>
-            <div style={{ color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '400' }}>{category}</div>
+            <div style={{ color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '400' }}>{category}</div> // 在庫登録した在庫のカテゴリー名を表示。
             <div style={{ color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '400', cursor: 'pointer' }} onClick={handleAddItem}>＋</div>
           </div>
           <div className={styles.searchContainer} style={{ left: '148px', top: '132px' }}>
